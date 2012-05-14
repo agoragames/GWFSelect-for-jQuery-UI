aadf7ec3a654ca91b33c7a88cf5855ff752a2d45
Jack Letourneau <jletourneau@agoragames.com>
2012-05-14 15:29:10 -0400

    Fetch full font list from Google (lazily).
    When widget is initialized with no arguments, it will retrieve and use
    the full set of available web fonts from Google. Optionally, the "fonts"
    param can specify a specific set of fonts for a widget to use.

--- javascript/jquery.gwfselect.js
+++ javascript/jquery.gwfselect.js
@@ -1,225 +1,25 @@
 (function ($) {
-    // FIXME: there's got to be a better way of getting the list of available
-    // fonts than hard-coding it into the plugin.
-    var fonts = [
-        "Aclonica",
-        "Allan",
-        "Annie Use Your Telescope",
-        "Anonymous Pro",
-        "Allerta Stencil",
-        "Allerta",
-        "Amaranth",
-        "Anton",
-        "Architects Daughter",
-        "Arimo",
-        "Artifika",
-        "Arvo",
-        "Asset",
-        "Astloch",
-        "Bangers",
-        "Bentham",
-        "Bevan",
-        "Bigshot One",
-        "Bowlby One",
-        "Bowlby One SC",
-        "Brawler",
-        "Buda:300",
-        "Cabin",
-        "Calligraffitti",
-        "Candal",
-        "Cantarell",
-        "Cardo",
-        "Carter One",
-        "Caudex",
-        "Cedarville Cursive",
-        "Cherry Cream Soda",
-        "Chewy",
-        "Coda",
-        "Coming Soon",
-        "Copse",
-        "Corben:700",
-        "Cousine",
-        "Covered By Your Grace",
-        "Crafty Girls",
-        "Crimson Text",
-        "Crushed",
-        "Cuprum",
-        "Damion",
-        "Dancing Script",
-        "Dawning of a New Day",
-        "Didact Gothic",
-        "Droid Sans",
-        "Droid Sans Mono",
-        "Droid Serif",
-        "EB Garamond",
-        "Expletus Sans",
-        "Fontdiner Swanky",
-        "Forum",
-        "Francois One",
-        "Geo",
-        "Give You Glory",
-        "Goblin One",
-        "Gravitas One",
-        "Gruppo",
-        "Hammersmith One",
-        "Holtwood One SC",
-        "Homemade Apple",
-        "Inconsolata",
-        "Indie Flower",
-        "IM Fell DW Pica",
-        "IM Fell DW Pica SC",
-        "IM Fell Double Pica",
-        "IM Fell Double Pica SC",
-        "IM Fell English",
-        "IM Fell English SC",
-        "IM Fell French Canon",
-        "IM Fell French Canon SC",
-        "IM Fell Great Primer",
-        "IM Fell Great Primer SC",
-        "Irish Grover",
-        "Irish Growler",
-        "Istok Web",
-        "Josefin Sans",
-        "Josefin Slab",
-        "Judson",
-        "Jura",
-        "Jura:500",
-        "Jura:600",
-        "Just Another Hand",
-        "Just Me Again Down Here",
-        "Kameron",
-        "Kenia",
-        "Kranky",
-        "Kreon",
-        "Kristi",
-        "La Belle Aurore",
-        "Lato:100",
-        "Lato:100italic",
-        "Lato:300",
-        "Lato",
-        "Lato:bold",
-        "Lato:900",
-        "League Script",
-        "Lekton",
-        "Limelight",
-        "Lobster",
-        "Lobster Two",
-        "Lora",
-        "Love Ya Like A Sister",
-        "Loved by the King",
-        "Luckiest Guy",
-        "Maiden Orange",
-        "Mako",
-        "Maven Pro",
-        "Maven Pro:500",
-        "Maven Pro:700",
-        "Maven Pro:900",
-        "Meddon",
-        "MedievalSharp",
-        "Megrim",
-        "Merriweather",
-        "Metrophobic",
-        "Michroma",
-        "Miltonian Tattoo",
-        "Miltonian",
-        "Modern Antiqua",
-        "Monofett",
-        "Molengo",
-        "Mountains of Christmas",
-        "Muli:300",
-        "Muli",
-        "Neucha",
-        "Neuton",
-        "News Cycle",
-        "Nixie One",
-        "Nobile",
-        "Nova Cut",
-        "Nova Flat",
-        "Nova Mono",
-        "Nova Oval",
-        "Nova Round",
-        "Nova Script",
-        "Nova Slim",
-        "Nova Square",
-        "Nunito:light",
-        "Nunito",
-        "OFL Sorts Mill Goudy TT",
-        "Old Standard TT",
-        "Open Sans:300",
-        "Open Sans",
-        "Open Sans:600",
-        "Open Sans:800",
-        "Open Sans Condensed:300",
-        "Orbitron",
-        "Orbitron:500",
-        "Orbitron:700",
-        "Orbitron:900",
-        "Oswald",
-        "Over the Rainbow",
-        "Reenie Beanie",
-        "Pacifico",
-        "Patrick Hand",
-        "Paytone One",
-        "Permanent Marker",
-        "Philosopher",
-        "Play",
-        "Playfair Display",
-        "Podkova",
-        "PT Sans",
-        "PT Sans Narrow",
-        "PT Sans Narrow:regular,bold",
-        "PT Serif",
-        "PT Serif Caption",
-        "Puritan",
-        "Quattrocento",
-        "Quattrocento Sans",
-        "Radley",
-        "Raleway:100",
-        "Redressed",
-        "Rock Salt",
-        "Rokkitt",
-        "Ruslan Display",
-        "Schoolbell",
-        "Shadows Into Light",
-        "Shanti",
-        "Sigmar One",
-        "Six Caps",
-        "Slackey",
-        "Smythe",
-        "Sniglet:800",
-        "Special Elite",
-        "Stardos Stencil",
-        "Sue Ellen Francisco",
-        "Sunshiney",
-        "Swanky and Moo Moo",
-        "Syncopate",
-        "Tangerine",
-        "Tenor Sans",
-        "Terminal Dosis Light",
-        "The Girl Next Door",
-        "Tinos",
-        "Ubuntu",
-        "Ultra",
-        "Unkempt",
-        "UnifrakturCook:bold",
-        "UnifrakturMaguntia",
-        "Varela",
-        "Varela Round",
-        "Vibur",
-        "Vollkorn",
-        "VT323",
-        "Waiting for the Sunrise",
-        "Wallpoet",
-        "Walter Turncoat",
-        "Wire One",
-        "Yanone Kaffeesatz",
-        "Yanone Kaffeesatz:300",
-        "Yanone Kaffeesatz:400",
-        "Yanone Kaffeesatz:700",
-        "Yeseva One",
-        "Zeyada"
-    ];
+    var GWFFonts = [];
     var loadedFonts = {};
+    var fontFetch = (function () {
+        var fontURL = 'https://www.googleapis.com/webfonts/v1/webfonts',
+            callbacks = [],
+            fetching = false;
+        var fetch = function () {
+            $.getJSON(fontURL, function (data, status, req) {
+                $.each(data.items, function () { GWFFonts.push(this.family); });
+                $.each(callbacks, function () { this.call(null); });
+                console.log(GWFFonts);
+            });
+            fetching = true;
+        };
+        return {
+            init: function (fn) {
+                callbacks.push(fn);
+                fetching || fetch();
+            }
+        };
+    })();
     $.widget('mlg.gwfselect', {
         options: {
             // None yet...
@@ -242,18 +42,25 @@
             this._selectFontListItem(randomFont);
         },
         _create: function () {
-            var opt = this.options,
-                el = this.element;
+            var self = this,
+                opt = this.options,
+                el = this.element,
+                useFonts = opt.fonts || GWFFonts;
             el.addClass('gwf-input')
                 .attr('readonly', 'readonly')
                 .wrap($('<div/>').addClass('gwf-wrapper'));
             this.wrapper = el.closest('.gwf-wrapper');
-            el.after(this._createFontList().hide());
-            this.toggle = $('<div/>')
-                .addClass('ui-icon ui-icon-triangle-1-s')
-                .insertAfter(el);
-            this._selectFontByName(el.val());
-            this._bindHandlers();
+            var createFontList = function () {
+                el.after(self._createFontList(useFonts).hide());
+                self._selectFontByName(el.val());
+                self.toggle = $('<div/>')
+                    .addClass('ui-icon ui-icon-triangle-1-s')
+                    .insertAfter(el);
+                self._bindHandlers();
+            };
+            useFonts.length ?
+                createFontList(useFonts) :
+                fontFetch.init(createFontList);
         },
         _bindHandlers: function () {
             var self = this,
@@ -303,10 +110,10 @@
             }
             return false;
         },
-        _createFontList: function () {
+        _createFontList: function (useFonts) {
             this.fontList = $('<ul/>').addClass('gwf-list');
             var self = this;
-            $.each(fonts, function (i, fontName) {
+            $.each(useFonts, function (i, fontName) {
                 $('<li/>')
                     .html(fontName)
                     .data('fontName', fontName)
